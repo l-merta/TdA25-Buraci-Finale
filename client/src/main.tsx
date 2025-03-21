253f5fd9be9698d4360d47d3c02b9fd46309713b
Student Cyber Games <guest00@NEREIS22.ad.fi.muni.cz>
2025-03-21 17:34:39 +0100

    React router dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './styles/index.css'
-import './styles/main.css'
+import './styles/index.css';
+import './styles/main.css';
 
-import App from './pages/App.tsx'
+import App from './pages/App.tsx';
+import Features from './pages/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/features' element={<Features />} />
+      </Routes>
+    </Router>
     <App />
   </StrictMode>,
 )
