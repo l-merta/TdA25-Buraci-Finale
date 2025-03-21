2a58f01df166df0c6f8e8f7e4b87cf08f29f14e7
l-merta <lukas.merta26@gmail.com>
2025-03-21 18:45:43 +0100

    server update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const http = require("http");
 const app = express();
 const PORT = process.env.PORT || 5200;
