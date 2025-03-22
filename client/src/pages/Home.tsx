5bb82ad8160e8b1b18ac65b17c5810a2e85b2441
l-merta <lukas.merta26@gmail.com>
2025-03-22 10:41:50 +0100

    RoomCode section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+//import { useState } from "react";
+
+import RoomCode from './../sections/RoomCode';
 
 const Home = () => {
-  const [roomCode, setRoomCode] = useState("");
-
   return (
     <main>
       <h1>Home stránka</h1>
-      <div style={{ marginTop: "20px" }}>
-        <input
-          type="text"
-          value={roomCode}
-          onChange={(e) => setRoomCode(e.target.value)}
-          placeholder="Enter room code"
-          style={{ marginRight: "10px", padding: "5px" }}
-        />
-        <Link to={`/mistnost/${roomCode}`}style={{ padding: "5px 10px" }}>Go to Room</Link>
-      </div>
+      <RoomCode />
     </main>
   );
 };
