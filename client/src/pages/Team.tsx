e1af749f40272e3b4b61e1bd911207f1668eaea9
Student Cyber Games <guest00@NEREIS22.ad.fi.muni.cz>
2025-03-22 16:30:46 +0100

    team page

---
+++ client/src/pages/Team.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Team: React.FC = () => {
+    return (
+        <main>
+            <div className="background-effect">
+                <div className="semicircle main-semicircle"></div>
+                <div className="semicircle moving-semicircle"></div>
+                <div className="semicircle moving-semicircle2"></div>
+            </div>
+            <section className="team-info">
+            <h1>O týmu - Buráci</h1>
+                <div className='text-team'>
+                    Jsme Buráci! Jsme tým složený z 3 členů, kteří se rozhodli vytvořit tuto webovou aplikaci. 2 Programátoři a jeden grafik se rozhodli založit tento oříškový tým tento rok za vidinou zlepšení svojich skills a získání nových zkušeností.
+                </div>
+                <div className='contact-info'>
+                    <div>Kontaktní údaje:</div>
+                    <div>Buráci s.r.o.</div>
+                    <div>Jablunkovská 295</div>
+                    <div>739 61 Třinec</div>
+                </div>
+            <img src="images/buraci-logo2.png" className='buraci-logo'/>
+            </section>
+        </main>
+    );
+};
+
+export default Team;
