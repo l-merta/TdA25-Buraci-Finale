fc8b50533fe79f695d0ffd97437858fdf0cb06b8
l-merta <lukas.merta26@gmail.com>
2025-03-22 10:29:14 +0100

    room code input

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,17 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home.tsx';
+//import Chat from './pages/Chat.tsx';
+import Features from './pages/Features.tsx';
+
 import './styles/index.css';
 import './styles/main.css';
-
-import Chat from './pages/Chat.tsx';
-import Features from './pages/Features.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path='/' element={<Chat />} />
+        <Route path='/' element={<Home />} />
         <Route path='/features' element={<Features />} />
       </Routes>
     </Router>
